b4a66750c6400d6302b2ca590feccb7efde3e7ff
blacktyger <blacktyg3r@gmail.com>
2021-05-19 09:32:58 +0100

    updates

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +10,10 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j6%zfwu1_6m^ez2cx^%abty6u&(s%_o=9^ens^wnl37%qx__1a'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = debug_mode
 
 ALLOWED_HOSTS = ["*"]
 
